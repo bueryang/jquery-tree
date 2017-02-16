160df602f96a74338ca5cd6cf3ad7abf8ba37f96
zhang wanchao <wb9@yingu.com>
2017-02-16 11:09:32 +0800

    add options

--- jQuery-tree-plugin.js
+++ jQuery-tree-plugin.js
@@ -1,44 +1,15 @@
+/*
+ * jQueryTree
+ * */
 (function ($, window) {
-    $.fn.contextMenu = function (settings) {
-        return this.each(function () {
+    $.fn.jQueryTree = function (settings) {
 
-            // Open context menu
-            $(this).on("contextmenu", function (e) {
-                // return native menu if pressing control
-                if (e.ctrlKey) return;
-
-                //open menu
-                var $menu = $(settings.menuSelector)
-                    .data("invokedOn", $(e.target))
-                    .show()
-                    .css({
-                        position: "absolute",
-                        left: getMenuPosition(e.clientX, 'width', 'scrollLeft'),
-                        top: getMenuPosition(e.clientY, 'height', 'scrollTop')
-                    })
-                    .off('click')
-                    .on('click', 'button', function (e) {
-                        $menu.hide();
-
-                        var $invokedOn = $menu.data("invokedOn");
-                        var $selectedMenu = $(e.target);
-
-                        settings.menuSelected.call(this, $invokedOn, $selectedMenu);
-                    });
-
-                return false;
-            });
-
-            //make sure menu closes on any click
-            $('body').click(function () {
-                $(settings.menuSelector).hide();
-            });
-        });
-
+        var bindTreeElement = $(this);
+        console.log(this);
         function getMenuPosition(mouse, direction, scrollDir) {
             var win = $(window)[direction](),
                 scroll = $(window)[scrollDir](),
-                menu = $(settings.menuSelector)[direction](),
+                menu = $("#jQuery-treeContextMenu")[direction](),
                 position = mouse + scroll;
 
             // opening menu would pass the side of the page
@@ -48,55 +19,122 @@
             return position;
         }
 
+        /*
+         * append menu
+         *
+         * */
+
+        var menuStr = "<ul id=\"jQuery-treeContextMenu\" style=\"display:none\">" +
+            "<li style=\"list-style-type:none;\"><button>new item</button></li>" +
+            " </ul>";
+        $('body').append(menuStr);
+
+
+        if (bindTreeElement.children('ul').length == 0) {
+            bindTreeElement.append("<ul class='root'></ul>");
+        }
+
+        /*
+         * load menu
+         * */
+
+        function writeTree(objArray) {
+            $.each(objArray, function (index, value) {
+                if (value.parentId == 0) {
+                    bindTreeElement.find('ul.root').append("<li cId='" + value.id + "'>" + "<span class='menu-item' title='Collapse this branc'><i></i> " + value.name + "</span><input type='checkbox'  value='true'>" + "</li>");
+                } else {
+                    bindTreeElement.find("li[cId='" + value.parentId + "']").addClass('parent_li');
+                    bindTreeElement.find("li[cId='" + value.parentId + "']").find("span:first > i").addClass("icon-minus-sign");
+                    if (bindTreeElement.find("li[cId='" + value.parentId + "']").find("ul:first").length == 0) {
+                        bindTreeElement.find("li[cId='" + value.parentId + "']").append("<ul><li cid='" + value.id + "'><span class='menu-item' title='Collapse this branc'><i class=''></i> " + value.name + "</span><input type='checkbox'  value='true'></li></ul>");
+                    } else {
+                        bindTreeElement.find("li[cId='" + value.parentId + "']").find("ul:first").append("<li cid='" + value.id + "'><span class='menu-item' title='Collapse this branc'><i class=''></i> " + value.name + "</span><input type='checkbox'  value='true'></li>");
+                    }
+                }
+                if (value.children != null) {
+                    writeTree(value.children);
+                }
+            });
+        }
+
+        $.ajax({
+            url: settings.dataUrl,
+            data: {},
+            dataType: "json",
+            success: function (resultData) {
+                if (resultData.children != null) {
+                    writeTree(resultData.children);
+                }
+            }
+        });
+
+        bindTreeElement.on('click', 'li.parent_li > span.menu-item', function (e) {
+            var children = $(this).parent('li.parent_li').find(' > ul > li');
+            if (children.is(":visible")) {
+                children.hide('fast');
+                $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+            } else {
+                children.show('fast');
+                $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+            }
+            e.stopPropagation();
+        });
+
+        bindTreeElement.on('contextmenu', 'li span.menu-item', function (e) {
+
+            console.log("menu");
+
+            // return native menu if pressing control
+            if (e.ctrlKey) return;
+            if (!settings.enableNewItem) return;
+
+            //open menu
+            var $menu = $("#jQuery-treeContextMenu")
+                .data("invokedOn", $(e.target))
+                .show()
+                .css({
+                    position: "absolute",
+                    left: getMenuPosition(e.clientX, 'width', 'scrollLeft'),
+                    top: getMenuPosition(e.clientY, 'height', 'scrollTop')
+                })
+                .off('click')
+                .on('click', 'button', function (e) {
+                    $menu.hide();
+
+                    var $invokedOn = $menu.data("invokedOn");
+                    var $selectedMenu = $(e.target);
+
+                    var msg = "You selected the menu item '" + $selectedMenu.text() +
+                        "' on the value '" + $invokedOn.text() + "'";
+
+                    var itemName = prompt("Please input", "New Item");
+
+                    if (itemName == null || itemName == "") {
+                        return false;
+                    }
+
+
+                    if ($invokedOn.parent().find("ul").length > 0) {
+                        var $newItem = $("<li> <span class='menu-item'><i class=''></i> " + itemName + "</span> <input type='checkbox'></li>");
+                        $invokedOn.parent().find("ul:first").append($newItem);
+                    } else {
+                        var $newItem = $("<ul><li> <span class='menu-item'><i class=''></i> " + itemName + "</span> <input type='checkbox'></li></ul>");
+                        $invokedOn.parent().addClass("parent_li");
+                        $invokedOn.find("i:first").addClass("icon-minus-sign");
+                        $invokedOn.parent().append($newItem);
+                    }
+
+                });
+
+            return false;
+        });
+
+        //make sure menu closes on any click
+        $('body').click(function () {
+            $("#jQuery-treeContextMenu").hide();
+        });
+
+
     };
 })(jQuery, window);
 
-
-$(function () {
-
-    /*
-     * append menu
-     *
-     *
-     * */
-
-
-    var menuStr = "<ul id=\"jQuery-treeContextMenu\" style=\"display:none\">" +
-        "<li style=\"list-style-type:none;\"><button>new item</button></li>" +
-        " </ul>";
-    $('body').append(menuStr);
-
-    $('.jQuery-tree li:has(ul)').addClass('parent_li').find(' > span.menu-item').attr('title', 'Collapse this branch');
-    $('.jQuery-tree li.parent_li > span.menu-item').on('click', function (e) {
-        var children = $(this).parent('li.parent_li').find(' > ul > li');
-        if (children.is(":visible")) {
-            children.hide('fast');
-            $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
-        } else {
-            children.show('fast');
-            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
-        }
-        e.stopPropagation();
-    });
-
-    $("ul .menu-item").contextMenu({
-        menuSelector: "#jQuery-treeContextMenu",
-        menuSelected: function (invokedOn, selectedMenu) {
-            var msg = "You selected the menu item '" + selectedMenu.text() +
-                "' on the value '" + invokedOn.text() + "'";
-
-            var itemName = prompt("Please input", "New Item");
-
-
-            if (invokedOn.parent().find("ul").length > 0) {
-                var $newItem = $("<li> <span class='menu-item'><i class='icon-leaf'></i> " + itemName + "</span> <input type='checkbox' checked='checked'></li>");
-                invokedOn.parent().find("ul:first").append($newItem);
-            } else {
-                var $newItem = $("<ul><li> <span class='menu-item'><i class='icon-leaf'></i> " + itemName + "</span> <input type='checkbox' checked='checked'></li></ul>");
-                invokedOn.parent().append($newItem);
-            }
-
-
-        }
-    });
-});
